fa21830c7c8ca10678d3de72f1b41b9efbfbc3fa
emrah2021 <82098888+emrah2021@users.noreply.github.com>
2021-05-16 18:32:17 +0300

    SortStackAccendingOrder
    Write a program to sort a stack in ascending order. You should not make any assumptions about how the stack is implemented. The following are the only functions that should be used to write this program: push | pop | peek | isEmpty.

---
+++ SortStackAccendingOrder.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+#include <list>
+#include <map>
+#include "Node.h"
+#include "Stack.h"
+#include "SetOfStacks.h"
+
+int main()
+{
+	Stack* s = new Stack();
+	StackNode* sn1 = new StackNode();
+	sn1->data = 2;
+	StackNode* sn2 = new StackNode();
+	sn2->data = 0;
+	StackNode* sn3 = new StackNode();
+	sn3->data = 4;
+	StackNode* sn4 = new StackNode();
+	sn4->data = 3;
+	StackNode* sn5 = new StackNode();
+	sn5->data = 1;
+	s->push(sn1);
+	s->push(sn2);
+	s->push(sn3);
+	s->push(sn4);
+	s->push(sn5);
+	s->pop;
+	s->push(sn5);
+	s = s->sort(s);
+	s->print(s);
+  
+  cin.get();
+	return 0;
+}
